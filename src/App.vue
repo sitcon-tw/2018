5137d0b80f57528638ddfcfac9ba946ada10148f
yoyo930021 <yoyo930021@gmail.com>
2018-01-18 15:37:21 +0800

    Add sass to project

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,6 @@ export default {
 }
 </script>
 
-<style>
-body {
-  margin: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+@import 'sass/main.sass'
 </style>
