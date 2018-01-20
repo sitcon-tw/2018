3ae3d12d330cc7a94a36503cdb460ed8bbd352f9
Maxwell-Alexius <destinydragon85614@gmail.com>
2018-01-20 15:50:48 +0800

    Place image into the agenda page and finish the basic layout structure of agenda page

--- src/pages/Agenda.vue
+++ src/pages/Agenda.vue
@@ -6,15 +6,10 @@
 
     <article id="lightning-talk">
       <div class="article-wrapper">
-        <h1>Lightning<br />Talk</h1>
+        <h1>Lightning <br/>Talk</h1>
         <p>
-Lightning talk,中文譯為「閃電秀」,顧名思義即為<br />
-「閃電一般短的演講」。包含準備時間在內,每位講者僅<br />
-有 3 分鐘的時間上台。用飛快的速度進行簡潔扼要的分<br />
-享,無論是對台上的講者或是台下的會眾,都是十分刺激<br />
-又轟動全場的體驗。Lightning talk 將會在年會當天早上<br />
-10:00 於白板區開放報名,先搶先贏,並在所有議程結束<br />
-後,閉幕前,在國際會議廳進行演講。
+          <img src="../assets/lighting_talk.png" alt="lightning talk" />
+          Lightning talk，中文譯為「閃電秀」，顧名思義即為「閃電一般短的演講」。包含準備時間在內，每位講者僅有 3 分鐘的時間上台。用飛快的速度進行簡潔扼要的分享，無論是對台上的講者或是台下的會眾，都是十分刺激又轟動全場的體驗。Lightning talk 將會在年會當天早上 10：00 於白板區開放報名，先搶先贏，並在所有議程結束後，閉幕前，在國際會議廳進行演講。
         </p>
       </div>
     </article>
@@ -22,19 +17,11 @@ Lightning talk,中文譯為「閃電秀」,顧名思義即為<br />
     <article id="tour">
       <div class="article-wrapper">
         <h1>導遊團</h1>
-        <p>
-第一次參加 Conference 嗎?第一次來到 SITCON 嗎?是個怕生的人嗎?到了現場人山人海,不知道<br />
-從何逛起?不管你是哪種,我們聽見你的聲音了。<br />
-<br />
-為了照顧第一次參與 Conference 的你,我們舉辦了 SITCON 導遊團,由走訪總召、幹部、講者、攤<br />
-位,帶您一步步了解 SITCON 的各個面向、運作模式。並認識學生計算機年會的核心概念!<br />
-<br />
-活動內容:將由 SITCON 的導遊們帶領報名會眾參觀各站,了解 SITCON。<br />
-每團人數:依照現場報名人數分組,最多10人一團。<br />
-導遊團活動所需時間:30 - 40 分鐘。<br />
-出發時間:中午 12:10。<br />
-報名地點及方式:需提早 10 分鐘至 2F 報到處旁沙發區現場報名。<br />
-        </p>
+        <p>第一次參加 Conference 嗎？第一次來到 SITCON 嗎？是個怕生的人嗎？到了現場人山人海，不知道從何逛起？不管你是哪種，我們聽見你的聲音了。</p>
+
+        <p>為了照顧第一次參與 Conference 的你，我們舉辦了 SITCON 導遊團，由走訪總召、幹部、講者、攤位，帶您一步步了解 SITCON 的各個面向、運作模式。並認識學生計算機年會的核心概念！</p>
+
+        <p>活動內容：將由 SITCON 的導遊們帶領報名會眾參觀各站，了解 SITCON。 <br/>每團人數：依照現場報名人數分組，最多10人一團。 <br/>導遊團活動所需時間：30 - 40 分鐘。 <br/>出發時間：中午 12:10。 <br/>報名地點及方式：需提早 10 分鐘至 2F 報到處旁沙發區現場報名。</p>
       </div>
     </article>
 
@@ -42,49 +29,30 @@ Lightning talk,中文譯為「閃電秀」,顧名思義即為<br />
       <div class="article-wrapper">
         <h1>Unconference</h1>
         <p>
-不同於由大會排定的議程,Unconference 讓會眾自主決<br />
-定想聽的講題規劃的議程。在會眾踴躍投票完想要分享的<br />
-主題之後,透過票選,每位講者有 20 分鐘在交誼廳展現<br />
-的機會。<br />
-<br />
-除此之外,Unconference 也歡迎會眾在交誼廳內進行任<br />
-何的社群活動,只要你有任何線下聚會的好點子,不妨現<br />
-<br />
-投稿方式<br />
-SITCON Unconference 投稿在 2/17 ~ 2/23,只要你是 SITCON 2018 的會眾,就能以報名序號填寫<br />
-Unconference 的投稿表單。<br />
-在投稿截止後,2/24 ~ 3/3 開放投票。只要報名 SITCON 2018 後,就可以憑著 SITCON 2018 票<br />
-券,填寫 Unconference 投票表單選出自己喜歡的議程或活動喔!<br />
-投票結束後將會在 3/4 公布選上的 Unconference,我們期待你在年會的分享。<br />
-注意事項<br />
-投稿及票選 Unconference 議程皆須以 SITCON 2018 票卷的 KKTIX 檢查碼為憑。<br />
-除有重大違規,否則議程組不會干涉 Unconference 投稿內容。
+          <img src="../assets/unconference.png" alt="unconference" />
+          不同於由大會排定的議程，Unconference 讓會眾自主決定想聽的講題規劃的議程。在會眾踴躍投票完想要分享的主題之後，透過票選，每位講者有 20 分鐘在交誼廳展現的機會。
         </p>
+
+        <p>除此之外，Unconference 也歡迎會眾在交誼廳內進行任何的社群活動，只要你有任何線下聚會的好點子，不妨現</p>
+
+        <p>投稿方式 <br /> SITCON Unconference 投稿在 2/17 ～ 2/23，只要你是 SITCON 2018 的會眾，就能以報名序號填寫 Unconference 的投稿表單。 <br/>在投稿截止後，2/24 ～ 3/3 開放投票。只要報名 SITCON 2018 後，就可以憑著 SITCON 2018 票券，填寫 Unconference 投票表單選出自己喜歡的議程或活動喔！ <br/>投票結束後將會在 3/4 公布選上的 Unconference，我們期待你在年會的分享。 <br />注意事項 <br />投稿及票選 Unconference 議程皆須以 SITCON 2018 票卷的 KKTIX 檢查碼為憑。除有重大違規，否則議程組不會干涉 Unconference 投稿內容。</p>
       </div>
     </article>
 
     <article id="other-event">
       <div class="article-wrapper">
         <h1>知識王</h1>
-        <p>
-在 SITCON 2018 首度嘗試知識王活動,想知道自己對 SITCON 或是資訊領域的了解多寡,那就趕緊<br />
-拿起行動裝置,開始挑戰吧!答不上來也別灰心,到攤位晃晃聊天,或許會意外發現謎腳喔!
-        </p>
+        <p>在 SITCON 2018 首度嘗試知識王活動，想知道自己對 SITCON 或是資訊領域的了解多寡，那就趕緊拿起行動裝置，開始挑戰吧！答不上來也別灰心，到攤位晃晃聊天，或許會意外發現謎腳喔！</p>
 
         <h1>社群攤位</h1>
-        <p>
-SITCON 今年新增了攤位活動,目的是在促進會眾跟學生社團或相關資訊社群交流。多元的活動讓你<br />
-在聽完議程意猶未盡之餘,更能充分交流!休息時間,別忘記到場外的攤位逛逛!
-        </p>
+        <p>SITCON 今年新增了攤位活動，目的是在促進會眾跟學生社團或相關資訊社群交流。多元的活動讓你在聽完議程意猶未盡之餘，更能充分交流！休息時間，別忘記到場外的攤位逛逛！</p>
       </div>
     </article>
 
     <article id="ground-game">
       <div class="article-wrapper">
-        <h1>大地遊戲<br /> <small>Badge 闖關</small></h1>
-        <p>
-請議程組自行補上文字
-        </p>
+        <h1>大地遊戲 <br /><small>Badge 闖關</small></h1>
+        <p>請議程組自行補上文字</p>
       </div>
     </article>
 
@@ -92,28 +60,15 @@ SITCON 今年新增了攤位活動,目的是在促進會眾跟學生社團或相
       <div class="article-wrapper">
         <h1>天使計畫</h1>
         <p>
-天使計畫是一個提供會眾們一對一交流機會的活動,在<br />
-此活動中您可以選擇成為「天使」或是「初心者」,主<br />
-辦方將會隨機配對天使與初心者,在告知您配對結果<br />
-後,天使與初心者就可以透過雙方同意的聯絡方式聯<br />
-絡,在年會當天也可以有更進一步的面對面交流!此活<br />
-動是為了讓剛接觸年會的人能夠藉由有經驗的人帶領而<br />
-更快了解年會!歡迎曾參加過年會的人成為天使,來幫<br />
-助各位初心者喔~<br />
-<br />
-詳細規則:<br />
-欲報名此活動的會眾,只要在購票表單上勾選參與天使計畫即可。<br />
-您可選擇您同意給予配對方的聯絡方式,但是信箱必須給予主辦方與配對方,以利通知配對結果。<br />
-<br />
-注意事項:<br />
-主辦方不保證配對成功<br />
-主辦方不處理配對相處狀況<br />
-請詳填資料,若資料不全可能會影響參與活動的資格<br />
-若有任何問題歡迎聯絡計畫負責人<br />
-<br />
-溫情喊話:<br />
-豐富您的自我介紹,能有機率性的提高配對率唷
+          <img id="angle-plan" src="../assets/angle_plan.png" alt="angle plan" />
+          天使計畫是一個提供會眾們一對一交流機會的活動，在此活動中您可以選擇成為「天使」或是「初心者」，主辦方將會隨機配對天使與初心者，在告知您配對結果後，天使與初心者就可以透過雙方同意的聯絡方式聯絡，在年會當天也可以有更進一步的面對面交流！此活動是為了讓剛接觸年會的人能夠藉由有經驗的人帶領而更快了解年會！歡迎曾參加過年會的人成為天使，來幫助各位初心者喔～
         </p>
+
+        <p>詳細規則： <br/>欲報名此活動的會眾，只要在購票表單上勾選參與天使計畫即可。您可選擇您同意給予配對方的聯絡方式，但是信箱必須給予主辦方與配對方，以利通知配對結果。</p>
+
+        <p>注意事項： <br/>主辦方不保證配對成功主辦方不處理配對相處狀況請詳填資料，若資料不全可能會影響參與活動的資格若有任何問題歡迎聯絡計畫負責人</p>
+
+        <p>溫情喊話： <br/>豐富您的自我介紹，能有機率性的提高配對率唷</p>
       </div>
     </article>
   </div>
@@ -125,15 +80,41 @@ SITCON 今年新增了攤位活動,目的是在促進會眾跟學生社團或相
   }
 </script>
 
-<style scoped>
-  article:nth-child(2n) {
-    color: white;
-    background-color: #62757D;
-    padding: 20pt 0;
-  }
-  article > div.article-wrapper {
-    text-align: left;
-    max-width: 960px;
-    margin: 0 auto;
-  }
+<style lang="sass" scoped>
+  article
+    padding: 20pt;
+
+    &:nth-child(2n)
+      color: white;
+      background-color: #62757D;
+
+    > div.article-wrapper
+      text-align: left;
+      max-width: 90%;
+      padding: 0 20px;
+      margin: 0 auto;
+      box-sizing: border-box;
+
+      > h1
+        margin: 20pt 0 10pt 0;
+        font-size: 60px;
+        line-height: 72px;
+
+      > p
+        margin-bottom: 30pt;
+        font-size: 32px;
+        line-height: 56px;
+        
+        &.half
+          width: 50%;
+
+        > img
+          float: right;
+          max-width: 50%;
+
+          &#angle-plan
+            margin-right: -15%;
+
+        + p
+          margin-top: 30pt;
 </style>
