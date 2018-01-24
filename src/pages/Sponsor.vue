686d7867a9bd2736288e82de7f355663b192ffec
yoyo930021 <yoyo930021@gmail.com>
2018-01-24 15:41:21 +0800

    Fix eslint error and .editorconfig error

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -1,55 +1,49 @@
 <template>
-        <div class="all">
-            <div class="co-host">
-                <h1 class="co-host_text">
-                    共同主辦
-                </h1>
-                <div class="co-host_text_1">
-                    <img src="../assets/sitcon.png">
-                </div>
-                <div class="co-host_text_1">
-                    <img src="../assets/sitcon.png">
-                </div>
-                <div class="co-host_text_1">
-                    <img src="../assets/sitcon.png">
-                </div>
-            </div>
-            <div class="co-host">
-                <h1 class="co-host_text">
-                    協辦單位
-                </h1>
-            </div>
-            <div class="co-host">
-                <h1 class="co-host_text">
-                    黃金級贊助
-                </h1>
-            </div>
-            <div class="co-host">
-                <h1 class="co-host_text">
-                    白銀級贊助
-                </h1>
-            </div>
-            <div class="co-host">
-                <h1 class="co-host_text">
-                    青銅級贊助
-                </h1>
-            </div>
-        </div>
+  <div class="all">
+    <div class="co-host">
+      <h1 class="co-host_text">
+        共同主辦
+      </h1>
+      <div class="co-host_text_1">
+        <img src="../assets/sitcon.png">
+      </div>
+      <div class="co-host_text_1">
+        <img src="../assets/sitcon.png">
+      </div>
+      <div class="co-host_text_1">
+        <img src="../assets/sitcon.png">
+      </div>
+    </div>
+    <div class="co-host">
+      <h1 class="co-host_text">
+        協辦單位
+      </h1>
+    </div>
+    <div class="co-host">
+      <h1 class="co-host_text">
+        黃金級贊助
+      </h1>
+    </div>
+    <div class="co-host">
+      <h1 class="co-host_text">
+        白銀級贊助
+      </h1>
+    </div>
+    <div class="co-host">
+      <h1 class="co-host_text">
+        青銅級贊助
+      </h1>
+    </div>
+  </div>
 
 </template>
 
 <script>
-    export default {
-    name: 'Sponsor'
+export default {
+  name: 'Sponsor'
 }
 </script>
 
-
-
-
-
-
-
-<style scoped>
+<style lang="sass" scoped>
 @import '../sass/sponsor.scss'
 </style>
--- src/pages/Staff.vue
+++ src/pages/Staff.vue
@@ -1,33 +1,31 @@
 <template>
 <div class="staff">
-    <h1>Staff Collection</h1>
-    <div class="group">
-        <h2>總召組</h2>
-        <div class="staff_card">
-            <div class="staff_photo">
-                <img src="http://sitcon.org/2016/image/staffs/10.jpg">
-            </div>
-            <p>MouseMs</p>
-        </div>
-        <div class="staff_card">
-            <div class="staff_photo">
-                <img src="http://sitcon.org/2016/image/staffs/10.jpg">
-            </div>
-            <p>MouseMs</p>
-        </div>
+  <h1>Staff Collection</h1>
+  <div class="group">
+    <h2>總召組</h2>
+    <div class="staff_card">
+      <div class="staff_photo">
+        <img src="http://sitcon.org/2016/image/staffs/10.jpg">
+      </div>
+      <p>MouseMs</p>
     </div>
-
-
+    <div class="staff_card">
+      <div class="staff_photo">
+        <img src="http://sitcon.org/2016/image/staffs/10.jpg">
+      </div>
+      <p>MouseMs</p>
+    </div>
+  </div>
 </div>
 
 </template>
 
 <script>
 export default {
-    name: 'Staff'
+  name: 'Staff'
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
 @import '../sass/staff.scss'
 </style>
