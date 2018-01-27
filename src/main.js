4442da3f98a64c5aad1cbaffc8cd3052b79c5671
Unknown <hsiuter880801@gmail.com>
2018-01-27 22:39:00 +0800

    Finish staffs second  version

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Components from './components/_index'
+import json from "../static/json/staff.json"
 
 Vue.config.productionTip = false
 
@@ -16,5 +17,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
 })
