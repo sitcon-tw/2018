40a28003ed99bcb2f44fc85c6729ae38fe7df08b
yoyo930021 <yoyo930021@gmail.com>
2018-02-01 22:57:14 +0800

    Fix sponsor style

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -1,14 +1,16 @@
 <template>
   <article class="all">
     <div class="container"><h2>贊助</h2></div>
-    <section class="brand color popular">
-      <div class="brand-image"></div>
+    <section class="brand color">
       <div class="container">
         <div class="content">
           <p>SITCON 學生計算機年會為學生自發組成籌辦的研討會，秉持著以學生為主軸的核心價值，提供一個經驗交流與資訊技術實務分享的平台，進而達到「學以致用、教學相長」的目標。</p>
           <p>建構如此的交流平台，需要多人的努力及貢獻，亦十分需要在資金上的支持。<br>希望能藉由您的一份力量，來促成活動的舉行，讓更多學生能在舞台上展現自我成長。<br>歡迎您參與贊助，支持 SITCON、讓我們將每年的活動辦得盡善盡美！</p>
           <p>個人贊助辦法：<a href="https://sitcon.kktix.cc/events/sitcon2018-individual-sponsor" target="_blank">連結</a></p>
           <p>企業贊助歡迎聯繫: contact@sitcon.org</p>
+        </div>
+        <div class="image">
+          <img src="" alt="個人贊助" />
         </div>
       </div>
     </section>
@@ -54,7 +56,10 @@ section
   text-align: left
   position: relative
   color: #585858
+  font-size: 0px
   .content
+    display: inline-block
+    vertical-align: middle
     width: 55%
     h2
       font-size: 22px
@@ -73,16 +78,21 @@ section
       font-weight: 550
       margin-top: 14px
       margin-bottom: 14px
-  .brand-image
-    position: absolute
-    top: 0
-    bottom: 0
-    right: 0
-    width: 35%
-    background-position: left center
-    background-size: 110% auto
-    background-repeat: no-repeat
+  .image
+    display: inline-block
+    vertical-align: middle
+    width: 45%
+    img
+      display: block
+      margin: 0 auto
+      width: 90%
 .color
   background-color: $main-color
   color: white
+@media all and (max-width: 1000px)
+  .brand
+    .content
+      width: 100%
+    .image
+      width: 100%
 </style>
