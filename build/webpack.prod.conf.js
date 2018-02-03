5648755df657e1e6b10d17cea554512545344a51
yoyo930021 <yoyo930021@gmail.com>
2018-02-03 21:58:01 +0800

    Modify favicon icon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -117,10 +117,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       {
         from: path.resolve(__dirname, '../cfp'),
         to: config.build.assetsCfpDirectory
-      },
-      {
-        from: path.resolve(__dirname, '../favicon.ico'),
-        to: config.build.assetsRoot
       }
     ])
   ]
