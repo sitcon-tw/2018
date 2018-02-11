8c2e347a8f292fb78940a26ce90e6bb846e1d901
yoyo930021 <yoyo930021@gmail.com>
2018-02-11 12:24:11 +0800

    Fix subtable avatar

--- src/components/SubTable.vue
+++ src/components/SubTable.vue
@@ -67,8 +67,8 @@ export default {
     openBox (sub) {
       this.subSubject = sub.subject
       this.subslides = sub.slides
-      this.subAvatar = 'http://sitcon.org/2018/static/img/speaker/' + sub.speaker.avatar
-      // this.subAvatar = ''
+      if (sub.speaker.avatar !== '') this.subAvatar = 'http://sitcon.org/2018/static/img/speaker/' + sub.speaker.avatar
+      else this.subAvatar = ''
       this.subBio = sub.speaker.bio
       this.subSpeakerName = sub.speaker.name
       this.subSummary = sub.summary
