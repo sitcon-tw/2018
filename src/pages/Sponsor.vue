b368d0713cbbfb1e2046e2929475bce20db5acbe
Denny Huang <denny0223@gmail.com>
2018-02-22 00:14:50 +0800

    Sponsor page use SITCON full name

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -51,7 +51,7 @@ export default {
           'title': '共同主辦',
           'item': [
             {
-              name: 'SITCON',
+              name: 'SITCON 學生計算機年會籌備團隊',
               icon: './static/img/sponsor/sitcon.png',
               summary: 'SITCON（Students\' Information Technology Conference, 學生計算機年會）是自 2013 年由一群學生自主性地發起，是一個以在學學生為主體的學生社群。以自發性的學習為基礎，秉持開源、創新與實作的核心理念。<br><br>我們希望藉由每年三月固定的研討會（SITCON）、每學期的 hackathon （駭客世代 HackGen）、每年暑假的夏令營及兩個禮拜定期聚會的方式，將全台灣各地對於資訊科技相關領域有興趣的學生們串聯起來，透過彼此共享、交流來切磋精進，砥礪自主自學的風氣。'
             },
